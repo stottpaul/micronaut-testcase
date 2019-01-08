1c82cc2c437d7e1b5589bdcfd0496d8aee63a24a
Paul Stott <paul.stott@johnlewis.co.uk>
2019-01-08 16:57:48 +0000

    Add mock beans

--- src/main/kotlin/com/testfailure/SampleController.kt
+++ src/main/kotlin/com/testfailure/SampleController.kt
@@ -3,12 +3,22 @@ package com.testfailure
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import javax.inject.Singleton
 
 @Controller("/test")
-class SampleController {
-
+class SampleController(private val service: Service)  {
     @Get(uri = "/hello", processes = arrayOf(MediaType.TEXT_PLAIN))
-    internal fun hello(): String {
-        return "Hello World"
+    fun hello(): String {
+        return service.getMessage()
     }
+}
+
+interface Service {
+    fun getMessage(): String
+}
+
+@Singleton
+class SampleService : Service {
+    override fun getMessage() = "Hello World"
+
 }
--- src/test/kotlin/com/testfailure/AnotherControllerTest.kt
+++ src/test/kotlin/com/testfailure/AnotherControllerTest.kt
@@ -3,22 +3,32 @@ package com.testfailure
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.annotation.MockBean
+import io.mockk.every
+import io.mockk.mockk
 import junit.framework.Assert.assertTrue
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
 @MicronautTest
-class SampleControllerTest {
+class AnotherControllerTest {
 
     @Inject
-    @field:Client( "/")
+    @field:Client("/")
     lateinit var client: RxHttpClient
+
+    @Inject
+    lateinit var service: Service
 
     @Test
     fun `should return hello world`() {
+//        `when`(service.getMessage()).thenReturn("Something else")
+        every { service.getMessage() }.returns("Something else")
         val response: String = client.toBlocking().retrieve("/test/hello")
-        assertTrue(response.equals("Hello World"))
+        assertTrue(response == "Something else")
     }
 
+    @MockBean(SampleService::class)
+    fun mockService(): Service = mockk()
 }
 
